c1dbdff1ebd85a157d2c867d26b0a3e45e23d84c
Petr Kotov <ipetrkotov@gmaiil.com>
2023-06-21 09:29:24 +0300

    "Сделал 1.1.5"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -13,7 +13,12 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
+        userService.saveUser("Dima","Domestos",(byte) 33);
         userService.saveUser("Petr","Kotov",(byte) 27);
+        userService.saveUser("Laura","Kotova", (byte) 25);
+        userService.saveUser("Kolya","Dmitriev",(byte) 24);
         userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
